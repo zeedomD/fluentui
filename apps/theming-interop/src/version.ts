735cee7fefc6a805037d5914a2a8dd6515f7857a
Marija Najdova <mnajdova@gmail.com>
2020-02-21 11:22:44 +0100

    wip

---
+++ apps/theming-interop/src/version.ts
@@ -0,0 +1,4 @@
+// theming-interop@7.0.0
+// Do not modify this file, the file is generated as part of publish. The checked in version is a placeholder only.
+import { setVersion } from '@uifabric/set-version';
+setVersion('theming-interop', '7.0.0');
